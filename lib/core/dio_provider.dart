68757dc66c6665e5fd48abdb8c762e88a66974c0
navneet.kshatriya <navneet.kshatriya@excellentwebworld.in>
2026-05-11 23:36:36 +0530

    Implemented provider code

--- lib/core/dio_provider.dart
+++ lib/core/dio_provider.dart
@@ -7,16 +7,20 @@ final tokenStorageProvider = Provider<TokenStorage>((ref) => TokenStorage());
 
 final dioProvider = Provider<Dio>((ref){
   final tokenStorage = ref.watch(tokenStorageProvider);
-
   final dio = Dio(
     BaseOptions(
-      baseUrl: 'https://jsonplaceholder.typicode.com',
+      // baseUrl: 'https://jsonplaceholder.typicode.com',
+      baseUrl: 'https://dummyjson.com',
       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(seconds: 30),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
     ),);
 
-  dio.interceptors.add(AuthInterceptor(dio, tokenStorage));
-  dio.interceptors.add(LogInterceptor(responseBody: true));
+  dio.interceptors.add(AuthInterceptor(tokenStorage, dio));
+  dio.interceptors.add(LogInterceptor(requestBody: true,responseBody: true));
 
   ref.onDispose(() => dio.close());
   return dio;
--- lib/features/posts/providers/post_provider.dart
+++ lib/features/posts/providers/post_provider.dart
@@ -1,37 +1,12 @@
-import 'package:dio/dio.dart';
-import 'package:flutter_not_ai/core/dio_provider.dart';
-import 'package:flutter_not_ai/features/posts/repository/post_repository.dart';
+// lib/features/posts/providers/post_provider.dart
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/repository/app_repository.dart';
 import '../models/post.dart';
 
-// navy: whats the usage of this file provider, if it is related to di
-// then why dont we ues it for dio and tokenstorage?
-
-
-
-final postRepositoryProvider = Provider<PostRepository>((ref){
-  final dio = ref.watch(dioProvider);
-  return PostRepository(dio);
-});
-
 final postsProvider = FutureProvider.autoDispose<List<Post>>((ref) async {
-  final repository = ref.watch(postRepositoryProvider);
-  return repository.fetchPosts();
+  final result = await ref.read(appRepositoryProvider).getPosts();
+  return result.fold(
+        (error) => throw error,   // home_view.dart when(error:) catches this
+        (posts)  => posts,
+  );
 });
-
-
-
-
-
-// navy - uncommented below code and added above code
-// Provide the ApiService (singleton)
-/*
-final apiServiceProvider = Provider<ApiService>((ref) {
-  return ApiService();
-});
-
-// FutureProvider that automatically fetches posts
-final postsProvider = FutureProvider<List<Post>>((ref) async {
-  final apiService = ref.watch(apiServiceProvider);
-  return apiService.fetchPosts();
-});*/
