05e0f2c16a55afeba3098e5ad162aa02162d737f
navneet.kshatriya <navneet.kshatriya@excellentwebworld.in>
2026-05-11 23:35:33 +0530

    Implemented Secure Token

--- lib/core/token_storage.dart
+++ lib/core/token_storage.dart
@@ -1,20 +1,27 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenStorage {
+  static const _storage    = FlutterSecureStorage();
+  static const _accessKey  = 'access_token';
+  static const _refreshKey = 'refresh_token';
 
-  static const _storage = FlutterSecureStorage();
-
-  Future<void> saveToken(String token) async {
-    await _storage.write(key: 'access_token', value: token);
+  Future<void> saveTokens({
+    required String accessToken,
+    String? refreshToken,
+  }) async {
+    await _storage.write(key: _accessKey, value: accessToken);
+    if (refreshToken != null) {
+      await _storage.write(key: _refreshKey, value: refreshToken);
+    }
   }
 
-  Future<String?> getToken() async {
-    return await _storage.read(key: 'access_token');
+  Future<String?> getAccessToken()  async => _storage.read(key: _accessKey);
+  Future<String?> getRefreshToken() async => _storage.read(key: _refreshKey);
+
+  Future<bool> hasToken() async {
+    final t = await getAccessToken();
+    return t != null && t.isNotEmpty;
   }
 
-  Future<void> clearTokens() async{
-    await _storage.deleteAll();
-  }
-
-
+  Future<void> clearTokens() async => _storage.deleteAll();
 }
